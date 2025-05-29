6b837d7ee7de4e08057b2e96441a4a6a5120cd1d
Rouie Ilustrisimo <ilustrisimo.rouie@gmail.com>
2025-05-29 23:14:38 +0800

    to vercel

--- src/server.js
+++ src/server.js
@@ -17,7 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://launch.gravitypointmedia.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
