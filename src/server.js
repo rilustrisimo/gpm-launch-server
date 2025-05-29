ca8432c6fd25aa1e9aa83941f837f2e57039beb7
Rouie Ilustrisimo <ilustrisimo.rouie@gmail.com>
2025-05-29 23:23:29 +0800

    to vercel CORS

--- src/server.js
+++ src/server.js
@@ -21,23 +21,16 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://launch.gravitypointmedia.com'],
+  origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Handle OPTIONS preflight requests
-app.options('*', cors({
-  origin: ['https://launch.gravitypointmedia.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(corsMiddleware);
 
 // Routes
 app.use('/api/auth', authRoutes);
