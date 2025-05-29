77b98f4de9ea9a9325c9db52b9468072f361e6e7
Rouie Ilustrisimo <ilustrisimo.rouie@gmail.com>
2025-05-29 23:34:42 +0800

    to vercel CORS

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+// Explicitly require mysql2 to ensure it's loaded
+require('mysql2');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.js')[env];
